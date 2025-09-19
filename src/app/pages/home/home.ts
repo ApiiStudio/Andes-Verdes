7a34492dd3aa02d2cc163603683f192934936883
joaquin <joaquinnarvay600@gmail.com>
2025-09-19 09:37:37 -0300

    se agregaron buscador y marco de mapa

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
-import { Footer } from '../../shared/footer/footer';
-import { Navbar } from '../../shared/navbar/navbar';
+import { Footer } from "../../shared/footer/footer";
+import { Navbar } from "../../shared/navbar/navbar";
 
 @Component({
   selector: 'app-home',
-  imports: [Navbar,Footer],
   templateUrl: './home.html',
-  styleUrl: './home.css'
+  styleUrls: ['./home.css'],
+  imports: [Footer, Navbar]
 })
 export class Home {
+  searchText = '';
 
+  onSearchChange(evt: Event) {
+    this.searchText = (evt.target as HTMLInputElement).value || '';
+    console.log('Buscar:', this.searchText);
+    // aquí puedes emitir o filtrar datos si luego agregas lista
+  }
 }
